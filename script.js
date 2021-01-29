6aae8b922b9bb12186ff1d71d92e8940eb65e4ae
HuzaifaKhalid2 <huzaifa.khalid@ocsbstudent.ca>
2021-01-29 03:34:25 +0000

    we got em 3.0

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function myFunction () {
   riddle2 = document.getElementById('my-text2').value
   if ((riddle1 === 'david') && (riddle2 === 'penny')) {
     alert('100/100 Congratulations! You got all the answers right! GREAT JOB :)')
-  } else { 
+  } else {
     alert(' 0/100 ohhh dear sorry but you have failed :(')
   } alert('If you passed congratulations your done but if you didnt then: if at first you dont suceed try try again or this fate is guaranteed')
 }
